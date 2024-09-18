6b28fdd139254cffdebf33438b9d00ce9bbbc40e
Aretemii Korshunov <56575740+TvoiaJopa@users.noreply.github.com>
2024-09-18 13:20:00 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,18 +5,7 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-            string teksti = "1";
-            int luku;
-
-            luku = int.Parse(teksti);
-            Console.WriteLine(teksti + teksti);
-            Console.WriteLine(luku + luku);
-
-            string teksti2;
-
-            teksti2 = Convert.ToString(luku);
-
-            Console.WriteLine(teksti2 + teksti2);
+          
 
         }
     }
